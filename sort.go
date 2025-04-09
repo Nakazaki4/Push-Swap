60a5c3b1ebcb7be28cabc64eabe8944690bbb865
Nakazaki4 <Nakazakinakazaki4@gmail.com>
2025-04-09 22:58:30 +0100

    Closer this time

--- sort.go
+++ sort.go
@@ -1,35 +1,80 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 func (op *StackOperator) Sort() {
-	isOneDigit := true
 
-	for _, d := range op.stackA.items {
-		if d > 9 {
-			isOneDigit = false
+	isSorted := false
+
+	for !isSorted {
+
+		if op.stackA.Size() <= 6 {
+			op.CyclicSort(op.stackA)
+			fmt.Println(op.stackA.items)
+		}
+		op.CyclicSort(op.stackB)
+		fmt.Println(op.stackB.items)
+
+		// Check if stack A is sorted
+		if op.isStackASorted() {
+			fmt.Println(op.stackA.items)
+			isSorted = true
 		}
 	}
 
-	if isOneDigit && op.stackA.Size() <= 6 {
-		op.CyclicSort()
-	}
 }
 
-func (op *StackOperator) CyclicSort() {
+func (op *StackOperator) CyclicSort(stack *Stack) {
 	mid := op.findMid()
-
-	for range op.stackA.Size() {
-		top, _ := op.stackA.Peek()
-		if top < mid {
-			op.PB()
-		} else {
-			op.RA()
+	if stack == op.stackA {
+		for range op.stackA.Size() {
+			top, _ := op.stackA.Peek()
+			if top < mid {
+				op.PB()
+			} else {
+				op.RA()
+			}
+			if op.stackA.Size() == 2 && top > mid {
+				op.SA()
+				break
+			}
+		}
+	} else if stack == op.stackB {
+		for range op.stackB.Size() {
+			top, _ := op.stackB.Peek()
+			if top > mid {
+				op.PA()
+			} else {
+				op.RB()
+			}
+			if op.stackB.Size() == 2 && top < mid {
+				op.SB()
+				op.PA()
+				op.PA()
+			}
 		}
 	}
-	mid = op.findMid()
-	
 
+}
+
+func (op *StackOperator) isStackASorted() bool {
+	if op.stackA.Size() <= 1 {
+		return true
+	}
+
+	aCopy := make([]int, op.stackA.Size())
+	copy(aCopy, op.stackA.items)
+
+	for i := 0; i < len(aCopy)-1; i++ {
+		if aCopy[i] > aCopy[i+1] {
+			return false
+		}
+	}
+
+	return true
 }
 
 func (op *StackOperator) findMid() int {
